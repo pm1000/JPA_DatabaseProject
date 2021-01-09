3c765e82c5ff63bda9d0c37d4c0beecc2b995d1f
Yannick Brilmayer <yannick.brilmayer@stud.h-da.de>
2021-01-09 10:06:29 -0800

    errors and warnings fixed

--- src/main/java/de/hda/fbi/db2/stud/impl/Lab03Impl.java
+++ src/main/java/de/hda/fbi/db2/stud/impl/Lab03Impl.java
@@ -2,13 +2,13 @@ package de.hda.fbi.db2.stud.impl;
 
 import de.hda.fbi.db2.stud.entity.*;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import javax.persistence.EntityTransaction;
 import javax.persistence.NoResultException;
 
 public class Lab03Impl extends de.hda.fbi.db2.api.Lab03Game {
 
-  private Scanner scanner;
   private javax.persistence.EntityManager em;
 
 
@@ -23,11 +23,6 @@ public class Lab03Impl extends de.hda.fbi.db2.api.Lab03Game {
   public void init() {
     super.init();
     em = lab02EntityManager.getEntityManager();
-    try {
-      scanner = new Scanner(new InputStreamReader(System.in, "UTF-8"));
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
 
   /**
@@ -76,20 +71,27 @@ public class Lab03Impl extends de.hda.fbi.db2.api.Lab03Game {
   @Override
   public Object interactiveGetOrCreatePlayer() {
 
-    System.out.print("Bitte einen Spielernamen eingeben: ");
+    // Prepare input variables.
     String name = "";
 
-    for (boolean isCorrect = false; !isCorrect; ) {
+    try {
+      // Prepare input.
+      Scanner scanner = new Scanner(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+      System.out.print("Bitte einen Spielernamen eingeben: ");
 
-      name = scanner.nextLine();
-      if (!name.isEmpty() || !name.isBlank()) {
-        isCorrect = true;
-      } else {
-        System.out.print("Name nicht korrekt. Bitte erneut eingeben: ");
+      // Read the name
+      for (boolean isCorrect = false; !isCorrect; ) {
+        name = scanner.nextLine();
+        if (!name.isEmpty() || !name.isBlank()) {
+          isCorrect = true;
+        } else {
+          System.out.print("Name nicht korrekt. Bitte erneut eingeben: ");
+        }
       }
+
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-
-
     return getOrCreatePlayer(name);
   }
 
@@ -148,82 +150,93 @@ public class Lab03Impl extends de.hda.fbi.db2.api.Lab03Game {
   public java.util.List<?> interactiveGetQuestions() {
 
     // Prepare input variables.
+    List<Category> categories = new ArrayList<>();
     int questionsPerCategory = 0;
     int amountCategories = 0;
 
-    // Get the questions per category.
-    System.out.print("Anzahl der Fragen pro Kategorie: ");
-    boolean isCorrect = false;
-    do {
-      try {
-        questionsPerCategory = scanner.nextInt();
-        if (questionsPerCategory > 0) { // More than zero questions per category.
-          isCorrect = true;
-        }
-      } catch (Exception e) {
-        System.out.print("Eingabe ist keine valide Zahl. Bitte Zahl eingeben: ");
-      }
-    } while (!isCorrect);
+    try {
+      // Prepare input
+      Scanner scanner = new Scanner(new InputStreamReader(System.in, StandardCharsets.UTF_8));
 
-
-    // Get the amount of categories asked.
-    System.out.print("Anzahl der Kategorien: ");
-    isCorrect = false;
-    do {
-      try {
-        amountCategories = scanner.nextInt();
-        if (amountCategories > 1) { // More than 2 categories.
-          isCorrect = true;
-        }
-      } catch (Exception e) {
-        System.out.print("Eingabe ist keine valide Zahl. Bitte Zahl eingeben: ");
-      }
-    } while (!isCorrect);
-
-    // Get all categories available.
-    List<?> allCategoriesList = em.createNamedQuery("Category.findAll").getResultList();
-
-    // print all categories with the corresponding ids and fill up a hashmap for easy access.
-    Map<Integer, Category> allCategoriesMap = new HashMap<>();
-    for (Object obj : allCategoriesList) {
-      Category cat = (Category) obj;
-      System.out.println("[" + cat.getCatID() + "] " + cat.getCategoryName());
-      allCategoriesMap.put(cat.getCatID(), cat);
-    }
-
-    // Get the categories from the user.
-    List<Category> categories = new ArrayList<>();
-    for (int i = 0; i < amountCategories; i++) {
-      System.out.print("Wähle deine " + (i + 1) + ". Kategorie per ID: ");
-
-      // Check the input of the user.
-      int catId = 0;
-      isCorrect = false;
+      // Get the questions per category.
+      System.out.print("Anzahl der Fragen pro Kategorie: ");
+      boolean isCorrect = false;
       do {
         try {
-          catId = scanner.nextInt();
-          isCorrect = true;
+          questionsPerCategory = scanner.nextInt();
+          if (questionsPerCategory > 0) { // More than zero questions per category.
+            isCorrect = true;
+          } else {
+            throw new Exception();
+          }
         } catch (Exception e) {
           System.out.print("Eingabe ist keine valide Zahl. Bitte Zahl eingeben: ");
         }
       } while (!isCorrect);
 
-      // Check if the category is already chosen.
-      if (!allCategoriesMap.containsKey(catId)
-            || categories.contains(allCategoriesMap.get(catId))) {
-        System.out.println("Kategorie nicht vorhanden oder schon ausgewählt.");
-        i--;
-      } else {
-        categories.add(allCategoriesMap.get(catId));
+
+      // Get the amount of categories asked.
+      System.out.print("Anzahl der Kategorien: ");
+      isCorrect = false;
+      do {
+        try {
+          amountCategories = scanner.nextInt();
+          if (amountCategories > 1) { // More than 2 categories.
+            isCorrect = true;
+          } else {
+            throw new Exception();
+          }
+        } catch (Exception e) {
+          System.out.print("Eingabe ist keine valide Zahl. Bitte Zahl eingeben: ");
+        }
+      } while (!isCorrect);
+
+      // Get all categories available.
+      List<?> allCategoriesList = em.createNamedQuery("Category.findAll").getResultList();
+
+      // print all categories with the corresponding ids and fill up a hashmap for easy access.
+      Map<Integer, Category> allCategoriesMap = new HashMap<>();
+      for (Object obj : allCategoriesList) {
+        Category cat = (Category) obj;
+        System.out.println("[" + cat.getCatID() + "] " + cat.getCategoryName());
+        allCategoriesMap.put(cat.getCatID(), cat);
       }
+
+      // Get the categories from the user.
+      for (int i = 0; i < amountCategories; i++) {
+        System.out.print("Wähle deine " + (i + 1) + ". Kategorie per ID: ");
+
+        // Check the input of the user.
+        int catId = 0;
+        isCorrect = false;
+        do {
+          try {
+            catId = scanner.nextInt();
+            isCorrect = true;
+          } catch (Exception e) {
+            System.out.print("Eingabe ist keine valide Zahl. Bitte Zahl eingeben: ");
+          }
+        } while (!isCorrect);
+
+        // Check if the category is already chosen.
+        if (!allCategoriesMap.containsKey(catId)
+              || categories.contains(allCategoriesMap.get(catId))) {
+          System.out.println("Kategorie nicht vorhanden oder schon ausgewählt.");
+          i--;
+        } else {
+          categories.add(allCategoriesMap.get(catId));
+        }
+      }
+
+      System.out.println("\n\n############################################################");
+      System.out.println("Fragen pro Kategorie: " + questionsPerCategory);
+      System.out.println("Anzahl Kategorien: " + amountCategories);
+      System.out.println("Ausgewählte Kategorien: " + categories.toString());
+      System.out.println("############################################################\n\n");
+
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-
-    System.out.println("\n\n############################################################");
-    System.out.println("Fragen pro Kategorie: " + questionsPerCategory);
-    System.out.println("Anzahl Kategorien: " + amountCategories);
-    System.out.println("Ausgewählte Kategorien: " + categories.toString());
-    System.out.println("############################################################\n\n");
-
     return getQuestions(categories, questionsPerCategory);
   }
 
@@ -239,7 +252,7 @@ public class Lab03Impl extends de.hda.fbi.db2.api.Lab03Game {
   public Object createGame(Object player, java.util.List<?> questions) {
     Game game = new Game((Player) player);
 
-    //create GameAnswerList
+    // Create GameAnswerList
     List<GameAnswer> gameAnswers = new ArrayList<>();
     for (Object question : questions) {
       gameAnswers.add(new GameAnswer(game, (Question) question));
@@ -257,11 +270,13 @@ public class Lab03Impl extends de.hda.fbi.db2.api.Lab03Game {
    */
   @Override
   public void playGame(Object game) {
-    // iterate trough each question and get the player answer
+
+    // Iterate trough each question and get the player answer.
     Game g = (Game) game;
     List<GameAnswer> gameAnswers = g.getAnswerList();
+
     for (GameAnswer gameAnswer : gameAnswers) {
-      //generate a random answer
+      // Generate a random answer.
       int answer = (int) ((Math.random() * (4 - 1)) + 1);
       gameAnswer.setPlayerAnswer(answer);
     }
@@ -275,52 +290,61 @@ public class Lab03Impl extends de.hda.fbi.db2.api.Lab03Game {
    */
   @Override
   public void interactivePlayGame(Object game) {
-    // iterate trough each question and get the player answer
-    Game g = (Game) game;
-    for (GameAnswer a : g.getAnswerList()) {
-      System.out.print("\nFrage: " + a.getQuestion().getQuestionText()
-                         + "\n Antwort 1: "
-                         + a.getQuestion().getAnswers().get(0)
-                         + "\n Antwort 2: "
-                         + a.getQuestion().getAnswers().get(1)
-                         + "\n Antwort 3: "
-                         + a.getQuestion().getAnswers().get(2)
-                         + "\n Antwort 4: "
-                         + a.getQuestion().getAnswers().get(3)
-                         + "\n Ihre Antwort: ");
 
-      // get the user input and check his input
-      int answer = 0;
-      boolean correctInput = false;
 
-      while (!correctInput) {
-        try {
-          answer = scanner.nextInt();
-          if (answer < 1 || answer > 4) {
-            throw new Exception();
+    try {
+      // Prepare input
+      Scanner scanner = new Scanner(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+
+      // Iterate trough each question and get the player answer
+      Game g = (Game) game;
+      for (GameAnswer a : g.getAnswerList()) {
+        System.out.print("\nFrage: " + a.getQuestion().getQuestionText()
+                           + "\n Antwort 1: "
+                           + a.getQuestion().getAnswers().get(0)
+                           + "\n Antwort 2: "
+                           + a.getQuestion().getAnswers().get(1)
+                           + "\n Antwort 3: "
+                           + a.getQuestion().getAnswers().get(2)
+                           + "\n Antwort 4: "
+                           + a.getQuestion().getAnswers().get(3)
+                           + "\n Ihre Antwort: ");
+
+        // get the user input and check his input
+        int answer = 0;
+        boolean correctInput = false;
+
+        while (!correctInput) {
+          try {
+            answer = scanner.nextInt();
+            if (answer < 1 || answer > 4) {
+              throw new Exception();
+            }
+            correctInput = true;
+          } catch (Exception e) {
+            System.out.println("Bitte geben Sie eine Zahl zwischen 1 und 4 ein:");
           }
-          correctInput = true;
-        } catch (Exception e) {
-          System.out.println("Bitte geben Sie eine Zahl zwischen 1 und 4 ein:");
+        }
+
+        //needed to for db
+        answer = answer - 1;
+
+        a.setPlayerAnswer(answer);
+
+        // Print the correect answer.
+        if (a.getQuestion().getCorrectAnswer() == answer) {
+          System.out.println("Die Antwort ist korrekt.");
+        } else {
+          System.out.println("Die Antwort ist nicht korrekt. Die korrekte Antwort ist "
+                               + (a.getQuestion().getCorrectAnswer() + 1));
         }
       }
 
-      //needed to for db
-      answer = answer - 1;
-
-      a.setPlayerAnswer(answer);
-
-      // Print the correect answer.
-      if (a.getQuestion().getCorrectAnswer() == answer) {
-        System.out.println("Die Antwort ist korrekt.");
-      } else {
-        System.out.println("Die Antwort ist nicht korrekt. Die korrekte Antwort ist "
-                             + (a.getQuestion().getCorrectAnswer() + 1));
-      }
+      //set end of the game
+      g.setEnd();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-
-    //set end of the game
-    g.setEnd();
   }
 
 
@@ -331,18 +355,19 @@ public class Lab03Impl extends de.hda.fbi.db2.api.Lab03Game {
    */
   @Override
   public void persistGame(Object game) {
-    //open transaction
+
+    // Open transaction
     EntityTransaction et = null;
 
     try {
       et = em.getTransaction();
       et.begin();
 
-      //first step: persist the game
+      // First step: persist the game
       Game g = (Game) game;
       em.persist(g);
 
-      //persist all answers
+      // Persist all answers
       List<GameAnswer> gameAnswers = g.getAnswerList();
       for (GameAnswer gameAnswer : gameAnswers) {
         em.persist(gameAnswer);
